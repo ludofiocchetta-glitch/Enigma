844a45f63a5670b6580e340d52381832923bf31a
Ludovica <ludofiocchetta@gmail.com>
2026-03-26 23:08:15 +0100

    modularità

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-
-function start() {
-        document.getElementById('homepage').classList.add('d-none');
-        document.getElementById('user').classList.remove('d-none');
-        document.getElementById('user').classList.add('fade-in');
-}
-
 function go() {
     const nome=document.getElementById('inputname').value;
     const avatar=document.querySelector('input[name="avatar"]:checked').value;
@@ -15,8 +8,39 @@ function go() {
     localStorage.setItem('username',nome);
     localStorage.setItem('avatar',avatar);
     console.log("Data saved"+nome+avatar+"\n");
-    document.getElementById('user').classList.add('d-none');
-    document.getElementById('room1').classList.remove('d-none');
-    void room1Div.offsetWidth;
-    document.getElementById('room1').classList.add('fade-in-slow');
+    window.location.href = "room1.html";
+}
+
+function mostraAtmosfera() {
+    alert("Non c'è tempo per viaggiare, Agente. La minaccia è qui in Europa...");
+}
+
+function mostraIndizio() {
+    alert("Ho trovato un appunto nascosto: la data cruciale è nascosta dietro la parola CYPHER.\n");
+}
+
+function apriEnigmaFinale() {
+    document.getElementById('codiceSoluzione').value = "";
+    var mioModal = new bootstrap.Modal(document.getElementById('enigmaModal'));
+    mioModal.show();
+}
+
+function controllaEnigma() {
+    const rispostaUtente = document.getElementById('codiceSoluzione').value;
+    const soluzioneCorretta = "1940"; 
+    
+    if (rispostaUtente === soluzioneCorretta) {
+        alert("Codice Accettato! Stai scappando verso la Room 2...");
+        bootstrap.Modal.getInstance(document.getElementById('enigmaModal')).hide();
+        
+        // trasporto alla Room 2
+        // window.location.href = "room2.html"; 
+    } else {
+        alert("Codice Errato, Agente. Riprova!");
+        document.getElementById('codiceSoluzione').value = ""; 
+    }
+}
+
+function mostraScatoloni() {
+    alert("Scartoffie, vecchi cavi elettrici e... una tazza di tè mezza vuota. Niente di utile per decifrare Enigma.");
 }
