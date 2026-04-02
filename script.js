c6394cd1945758787277f08e23de106e6668747a
Ludovica <ludofiocchetta@gmail.com>
2026-04-02 15:45:01 +0200

    modifiche room1

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ window.onload = function() {
         document.getElementById('testoMacchina').classList.add('cursore'); 
         avviaMissione();
     }
+    if (document.getElementById('testoMacchina1')) {
+        document.getElementById('testoMacchina1').classList.add('cursore'); 
+        inizioStanzaT();
+    }
     if (document.getElementById('avatarid')) {
         caricaAvatarInAngolo();
     }
@@ -96,12 +100,14 @@ function iniziaEscapeRoom() {
     
 }
 
-// Questo "ascolta" ogni volta che l'utente digita qualcosa nel rettangolo
-document.getElementById('inputname').addEventListener('input', function() {
-    // Appena scrive una lettera, togliamo il bordo rosso
-    this.classList.remove('is-invalid');
-    this.placeholder = ""; // Resettiamo anche il testo sfumato
-});
+const userName = document.getElementById('inputname');
+
+if (userName !== null) {
+    campoNome.addEventListener('input', function() {
+        this.classList.remove('is-invalid');
+        this.placeholder = ""; 
+    });
+}
 
 function caricaAvatarInAngolo() {
     const avatarname = localStorage.getItem('avatar');
@@ -127,29 +133,192 @@ function mostraMessaggio(titolo, testo) {
     var mioModalInfo = new bootstrap.Modal(document.getElementById('infoModal'));
     mioModalInfo.show();
 }
+
+
 /*FUNZIONI STANZA TURING*/
+//funzioni per il testo all'entrata della stanza
+function inizioStanzaT() {
+    const avatarName= localStorage.getItem('avatar');
+    let nomeAvatar = "";
+    if (avatarName === "detective1") {
+        nomeAvatar = "Alan Turing";
+    } else if (avatarName === "detective2") {
+        nomeAvatar = "Marie Curie";
+    } else if (avatarName === "detective3") {
+        nomeAvatar = "Albert Einstein";
+    } else if (avatarName === "detective4") {
+        nomeAvatar = "Ada Lovelace";
+    }    
+    const messaggio = `${nomeAvatar} sei entrato nel primo luogo della tua missione, esploralo bene! \nVai alla ricerca di tutti gli oggetti interessanti, conterranno indizi utili e domande per capire in che posto ti trovi e, soprattutto, per decifrare l'enigma finale. \nSolo rispondendo correttamente a tutto la tua avventura potrà continuare.`;
+    // digitazione automatica
+    const boxtesto = document.getElementById('testoMacchina1');
+    boxtesto.style.cursor = "pointer";
+    boxtesto.onclick = function() {
+        skipIntro = true;
+    }
+    scriviTestoT(messaggio, 0);
+
+}
+
+function scriviTestoT(testo, indice) {
+    const elemento = document.getElementById('testoMacchina1');
+
+    if (skipIntro) {
+        elemento.innerHTML = testo.replace(/\n/g, "<br>");
+        mostraBottoneFinaleT();
+        return;
+    }
+
+    if (indice < testo.length) {
+        let carattere = testo.charAt(indice);
+        if (carattere === '\n') {
+            document.getElementById('testoMacchina1').innerHTML += "<br>";
+        } else {
+            document.getElementById('testoMacchina1').innerHTML += carattere;
+        }
+        setTimeout(() => scriviTestoT(testo, indice + 1), 20);
+    } else {
+        mostraBottoneFinaleT();
+    }
+}
+
+function mostraBottoneFinaleT() {
+    document.getElementById('testoMacchina1').classList.remove('cursore');
+    const bottone = document.getElementById('btnEntra');
+    bottone.classList.remove('d-none');
+    bottone.classList.add('fade-in');
+}
+
+function iniziaEsplorazioneT() {
+    document.getElementById('introT').classList.add('d-none');
+    const avatar=document.getElementById('avatarcontenitore');
+    avatar.classList.remove('d-none');
+    avatar.classList.add('fade-in');
+}
+
+let countLavagna=0;
+let countTelefono=0;
+
+let enigmiRisolti= {
+    lavagna:false,
+    telefono:false
+};
+
+let oggettiEsplorati= {
+    mappamondo:false,
+    schedario:false,
+    cappello:false,
+    scatoloni:false
+};
+
 function mostraAtmosferaTuring() {
-    mostraMessaggio("Mappamondo", "Non c'è tempo per viaggiare, Agente. La minaccia è qui in Europa... esplora bene questa stanza.");
+    mostraMessaggio("Mappamondo", "I messaggi attraversano il continente… ma vengono compresi solo in un luogo. \n Non cercare lontano Agente.");
+    oggettiEsplorati.mappamondo=true;
 }
 
 function mostraIndizioTuring() {
-    mostraMessaggio("Schedario Chiuso", "Ci sei quasi, hai trovato un appunto segreto: la data cruciale è nascosta dietro la parola CYPHER.");
+    mostraMessaggio("Schedario Chiuso", "Ci sei quasi, hai trovato un appunto segreto: Le parole più importanti non sono scritte per intero.\n B_MBE");
+    oggettiEsplorati.schedario=true;
 }
 
 function mostraScatoloni() {
-    mostraMessaggio("Scatoloni Polverosi", "Scartoffie, vecchi cavi elettrici e... una tazza di tè mezza vuota. Niente di utile per decifrare Enigma, continua a cercare.");
+    mostraMessaggio("Scatoloni polverosi", "Scartoffie, appunti tecnici  e... una tazza di tè ormai fredda. \n Qualcuno ha lavorato qui a lungo.");
+    oggettiEsplorati.scatoloni=true;
 }
 
-function apriEnigmaFinale() {
-    document.getElementById('codiceSoluzione').value = "";
-    document.getElementById('codiceSoluzione').placeholder = "????";
-    var mioModal = new bootstrap.Modal(document.getElementById('enigmaModal'));
+function mostraCappello() {
+    mostraMessaggio("Cappello", "Un uomo che ha trasformato lettere in armi");
+    oggettiEsplorati.cappello=true;
+}
+
+/*FUNZIONE PER IL MODALE DINAMICO*/
+function apriModal(titolo, descrizione, richiesta, testoBottone, funzioneControllo, usaPlaceholder) {
+    document.getElementById('modalTitle').innerText = titolo;
+    document.getElementById('descrizione').innerText = descrizione;
+    document.getElementById('richiesta').innerText = richiesta;
+    
+    let inputField = document.getElementById('codiceSoluzione');
+    inputField.value = "";
+    inputField.placeholder = usaPlaceholder ? "?????" : "";
+    let btnConferma = document.getElementById('btnConferma');
+    btnConferma.innerText = testoBottone;
+    btnConferma.onclick = funzioneControllo;
+
+    var mioModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('enigmaModal'));
     mioModal.show();
 }
 
+function mostraLavagna() {
+    apriModal("Lavagna", "Se capisci la macchina, capisci tutto.\n GPKIOC",
+        "Decifra gli appunti: ", "Controlla", controllaLavagna,false);
+}
+
+function controllaLavagna() {
+    const risposta=document.getElementById('codiceSoluzione').value.trim().toLowerCase()
+    if (risposta=="enigma") {
+        bootstrap.Modal.getInstance(document.getElementById('enigmaModal')).hide();
+        mostraMessaggio("Accettato", "Geniale! hai decifrato il codice sulla lavagna. \n Puoi continuare a cercare.");
+        enigmiRisolti.lavagna=true;
+    } else {
+        countLavagna++;
+        if (countLavagna==2) {
+            document.getElementById('codiceSoluzione').value = "";
+            document.getElementById('codiceSoluzione').placeholder = "Indizio: spostati nell'alfabeto";
+        }
+        else if (countLavagna>=3) {
+            document.getElementById('codiceSoluzione').value = "";
+            document.getElementById('codiceSoluzione').placeholder = "Indizio: shift=2";
+        }
+        else {
+            document.getElementById('codiceSoluzione').value = "";
+            document.getElementById('codiceSoluzione').placeholder = "Riprova";
+        }
+    }
+}
+
+function mostraTelefono() {
+    apriModal("Telefono", "Ogni numero ha una voce. Ascoltala.\n 20-21-18-9-14-7",
+        "Decifra numero: ","Controlla", controllaTelefono,false);
+}
+
+function controllaTelefono() {
+    const risposta=document.getElementById('codiceSoluzione').value.trim().toLowerCase()
+    if (risposta=="turing") {
+        bootstrap.Modal.getInstance(document.getElementById('enigmaModal')).hide();
+        mostraMessaggio("Accettato", "Fantastico! hai capito il significato nascosto del numero. \n Continua la tua esplorazione.");
+        enigmiRisolti.telefono=true;
+    } else {
+        countTelefono++;
+        if (countTelefono==2) {
+            document.getElementById('codiceSoluzione').value = "";
+            document.getElementById('codiceSoluzione').placeholder = "Indizio: ogni lettera ha un posto";
+        }
+        else if (countTelefono>=3) {
+            document.getElementById('codiceSoluzione').value = "";
+            document.getElementById('codiceSoluzione').placeholder = "Indizio: A=1,B=2...";
+        }
+        else {
+            document.getElementById('codiceSoluzione').value = "";
+            document.getElementById('codiceSoluzione').placeholder = "Riprova";
+        }
+    }
+}
+
+function mostraEnigmaFinale() {
+    if (enigmiRisolti.telefono && enigmiRisolti.lavagna && oggettiEsplorati.mappamondo 
+    && oggettiEsplorati.cappello && oggettiEsplorati.scatoloni && oggettiEsplorati.schedario) {
+        apriModal("Macchina Enigma Intercettata","Agente, abbiamo decifrato parte del messaggio. Serve il nome della macchina che ha permesso di decifrare i messaggi nemici.",
+        "Inserisci il nome: ", "Decodifica la macchina", controllaEnigma,true);
+    }
+    else {
+        mostraMessaggio("Accesso Negato", "Agente, non sei ancora pronto per la decodifica finale. Ispeziona tutti gli oggetti nella stanza.");
+        return;
+    }
+}
+
 function controllaEnigma() {
-    const rispostaUtente = document.getElementById('codiceSoluzione').value;
-    const soluzioneCorretta = "1940";
+    const rispostaUtente = document.getElementById('codiceSoluzione').value.trim().toLowerCase();
+    const soluzioneCorretta = "bombe";
     
     if (rispostaUtente === soluzioneCorretta) {
         bootstrap.Modal.getInstance(document.getElementById('enigmaModal')).hide();
@@ -157,7 +326,7 @@ function controllaEnigma() {
         setTimeout(() => { window.location.href = "room2.html"; }, 3000);
     } else {
         document.getElementById('codiceSoluzione').value = "";
-        document.getElementById('codiceSoluzione').placeholder = "ERRATO. Riprova!";
+        document.getElementById('codiceSoluzione').placeholder = "Non è il nome che stiamo cercando. Riprova!";
     }
 }
 /*FUNZIONI STANZA CURIE*/
@@ -234,6 +403,10 @@ function mostraMappamondo() {
 
 function mostraAtmosferaLovelace() {
     mostraMessaggio("Camino", "Ti trovi nello studio di Ada Lovelace considerata la madre dell'informatica!");
+}
+
+function mostraOrologio() {
+    mostraMessaggio("Orologio", "Il tempo scorre veloce, corri ad esplorare!")
 }
 
 function mostraIndizioLovelace() {
