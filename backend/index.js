086bf9e04b41e9c3ca758c80870bd7c0404fbe77
Gaddo <gdt3tr@gmail.com>
2026-05-04 13:38:36 +0200

    navigazione quasi finita aggiunto controllo enigma

--- backend/index.js
+++ backend/index.js
@@ -21,13 +21,13 @@ const supabaseApiKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseApi, supabaseApiKey);
 
 // IMPORTANTE
-// numeri stanze: 0 start, 1 login, 2 mission, 3 turing, 4 curie, 5 einstein, 6 lovelace, 
+// numeri stanze: 0 start, 1 login, 2 mission, 3 turing, 4 curie, 5 einstein, 6 lovelace,
 // 7 final, 8 victory
 // nel database bisogna salvare il numero di stanza giusto già incrementato in cui rimandare
 // l'utente, cioè se un utente ha fatto solo il login salvo 2 per mandarlo a mission
 // se letto mission e cliccato inizia missione salvo 3
 // se ha risposto all'enigma finale di una stanza salvo come numero la stanza dopo seguendo
-// questa numerazione, se ha risposto solo alle domande intermedie salvo il numero di 
+// questa numerazione, se ha risposto solo alle domande intermedie salvo il numero di
 // quella stanza sempre con questa numerazione
 // in pratica dopo che l'utente si è registrato sta già alla 2
 
@@ -50,22 +50,24 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24, // 24 ore
       httpOnly: true,
     },
-  })
+  }),
 );
 
 app.use(express.json());
 
 // REGISTRAZIONE
-app.post("/api/register", async (req, res) => {
+app.post('/api/register', async (req, res) => {
   const { username, password, avatar } = req.body;
-    
-  if(!username || !password || !avatar) {
-      return res.status(400).json({error: `Tutti i campi sono obbligatori` });
+
+  if (!username || !password || !avatar) {
+    return res.status(400).json({ error: `Tutti i campi sono obbligatori` });
   }
-  if(password.length < 8) {
-      return res.status(400).json({error: `La password deve avere almeno 8 caratteri`});
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({ error: `La password deve avere almeno 8 caratteri` });
   }
-  
+
   const salt = 10;
   const password_hash = await bcrypt.hash(password, salt);
 
@@ -76,20 +78,22 @@ app.post("/api/register", async (req, res) => {
       .select();
 
     if (error) {
-        if(error.code == '23505') {
-            return res.status(400).json({error: "Username già esistente, inserire un altro username!"});
-        }
-        throw error;
+      if (error.code == '23505') {
+        return res.status(400).json({
+          error: 'Username già esistente, inserire un altro username!',
+        });
+      }
+      throw error;
     }
 
     const { data: progress, error: progress_error } = await supabase
       .from('progress')
-      .insert([{ username: username, room: 2, score : 0}])
+      .insert([{ username: username, room: 0, score: 0 }])
       .select();
 
     if (progress_error) throw progress_error;
 
-    const {data : inventory, error: inventory_error} = await supabase
+    const { data: inventory, error: inventory_error } = await supabase
       .from('inventory')
       .insert([{ id: username, notebook: [] }])
       .select();
@@ -98,13 +102,14 @@ app.post("/api/register", async (req, res) => {
 
     req.session.user = {
       username: data[0].id,
-      room: 2,
+      room: 0,
       inventory: [],
-      avatar: avatar
+      avatar: avatar,
     };
 
-    return res.status(201).json({ message: 'Utente creato con successo', data: req.session.user });
-
+    return res
+      .status(201)
+      .json({ message: 'Utente creato con successo', data: req.session.user });
   } catch (err) {
     console.error('Errore nella registrazione: ', err);
     return res.status(500).json({ errore: 'Errore interno del server' });
@@ -116,17 +121,17 @@ app.post('/api/login', async (req, res) => {
   const { username, password, avatar } = req.body;
   try {
     const { data: user, error } = await supabase
-        .from('User')
-        .select('id, username, password, avatar')
-        .eq('username', username)
-        .single();
-    
+      .from('User')
+      .select('id, username, password, avatar')
+      .eq('username', username)
+      .single();
+
     if (error || !user) {
-        return res.status(404).json({error: 'Utente inesistente. Registrati!'});
+      return res.status(404).json({ error: 'Utente inesistente. Registrati!' });
     }
 
-    if(!(await bcrypt.compare(password, user.password))) {
-        return res.status(401).json({error: 'Password errata'});
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: 'Password errata' });
     }
 
     const { data: game, error: game_error } = await supabase
@@ -137,7 +142,7 @@ app.post('/api/login', async (req, res) => {
       .single();
 
     if (game_error) {
-      console.error("Errore progressi:", game_error);
+      console.error('Errore progressi:', game_error);
       return res.status(500).json({ error: 'Errore database progressi' });
     }
 
@@ -148,67 +153,67 @@ app.post('/api/login', async (req, res) => {
       .single();
 
     if (inventory_error && inventory_error.code !== 'PGRST116') {
-      console.error("Errore inventario:", inventory_error);
+      console.error('Errore inventario:', inventory_error);
       return res.status(500).json({ error: 'Errore database inventario' });
     }
 
-    
     req.session.user = {
       username: user.id,
-      room: game.room,
+      room: game.room + 1, //ricordiamoci di incrementare la stanza di 1 per mandare l'utente alla stanza giusta
       inventory: inventory ? inventory.notebook : [],
-      avatar: user.avatar
+      avatar: user.avatar,
     };
 
-    return res.status(200).json({ message: 'Login effettuato', room: req.session.user.room });
-
+    return res
+      .status(200)
+      .json({ message: 'Login effettuato', room: req.session.user.room });
   } catch (err) {
-    console.error("Errore generico nel login:", err);
+    console.error('Errore generico nel login:', err);
     return res.status(500).json({ error: 'Errore interno del server' });
   }
 });
 
 // CONTROLLO SESSIONE
 app.get('/api/me', (req, res) => {
-    if(req.session.user){
-      res.json({
-        username: req.session.user.username,
-        room: req.session.user.room,
-        avatar: req.session.user.avatar
+  if (req.session.user) {
+    res.json({
+      username: req.session.user.username,
+      room: req.session.user.room,
+      avatar: req.session.user.avatar,
     });
-    }else{
-        res.status(401).json({error: "Non loggato"});
-    }
+  } else {
+    res.status(401).json({ error: 'Non loggato' });
+  }
 });
 
 // RESET PARTITA
 app.post('/api/reset-game', async (req, res) => {
-    const { username, nuovoAvatar } = req.body;
-    try {
-        await supabase
-            .from('User')
-            .update({ avatar: nuovoAvatar })
-            .eq('username', username);
-        await supabase
-            .from('progress')
-            .update({ room: 2, score: 0 })
-            .eq('user', username);
-        await supabase
-            .from('inventory')
-            .update({ notebook: [] })
-            .eq('username', username);
+  const { username, nuovoAvatar } = req.body;
+  try {
+    await supabase
+      .from('User')
+      .update({ avatar: nuovoAvatar })
+      .eq('username', username);
+    await supabase
+      .from('progress')
+      .update({ room: 0, score: 0 })
+      .eq('user', username);
+    await supabase
+      .from('inventory')
+      .update({ notebook: [] })
+      .eq('username', username);
 
-        if(req.session.user) {
-            req.session.user.avatar = nuovoAvatar;
-            req.session.user.room = 2;
-            req.session.user.inventory = [];
-        }
-
-        return res.status(200).json({ message: "Partita resettata" });
-    } catch (err) {
-        console.error("Errore nel reset:", err);
-        return res.status(500).json({ error: 'Impossibile resettare la partita' });
+    if (req.session.user) {
+      req.session.user.avatar = nuovoAvatar;
+      req.session.user.room = 0;
+      req.session.user.inventory = [];
     }
+
+    return res.status(200).json({ message: 'Partita resettata' });
+  } catch (err) {
+    console.error('Errore nel reset:', err);
+    return res.status(500).json({ error: 'Impossibile resettare la partita' });
+  }
 });
 
 // LOGOUT
@@ -217,17 +222,77 @@ app.post('/api/logout', (req, res) => {
     if (err) {
       return res.status(500).json({ error: 'Impossibile eseguire il logout' });
     }
-    
+
     res.clearCookie('connect.sid');
     return res.status(200).json({ message: 'Logout eseguito correttamente' });
   });
 });
 
-
 // UPDATE SCORE
 app.put('/api/update-score', async (req, res) => {
-    // Da implementare
+  const { username, newScore } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from('progress')
+      .update({ score: newScore })
+      .eq('username', username);
+  } catch (err) {
+    console.error("Errore nell'aggiornamento del punteggio:", err);
+    return res.status(500).json({ error: 'Errore interno del server' });
+  }
 });
+
+//Enigma risolto
+app.put('/api/room-completed', async (req, res) => {
+  const { username, newRoom } = req.body;
+  try {
+    const { data, error } = await supabase
+      .from('progress')
+      .update({ room: newRoom })
+      .eq('username', username);
+
+    req.session.user.room = newRoom + 1;
+    res
+      .status(200)
+      .json({ message: 'Stanza aggiornata', newRoom: newRoom + 1 });
+    //return res.redirect(`/index/room${newRoom + 1}`);
+  } catch (err) {
+    console.error("Errore nell'aggiornamento della stanza:", err);
+    return res.status(500).json({ error: 'Errore interno del server' });
+  }
+});
+
+// reindirizzare se in stanza non autorizzata
+
+//middleware per stanza sbagliata
+const wrongRoom = (req, res, next) => {
+  const requiredroom = parseInt(req.params.numero, 10);
+  if (req.session.user.room !== requiredroom) {
+    return res.redirect(`/index/room/${requiredroom}`);
+  }
+};
+
+//middleware per check login
+const loggedIn = (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect(`/index/room/${req.session.user.room}`);
+  }
+  next();
+};
+
+app.get('/login', loggedIn, (req, res) => {
+  res.sendFile(path.join(root, 'frontend', 'pages', 'login.html'));
+});
+
+// non accede a mission poichè la sessione parte dalla room 1 e lascio mission al frontrnd
+app.get('/index/room/:numero', wrongRoom, (req, res) => {
+  const numeroStanza = req.params.numero;
+  res.sendFile(
+    path.join(root, 'frontend', 'pages', `room${numeroStanza}.html`),
+  );
+});
+
+//classifica (primi 5 utenti con punteggio più alto)
 
 // AVVIO SERVER
 app.listen(port, host, () => {
